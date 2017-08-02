983489ad76817a4b25efe8a478187f1f55f122b8
zhiweiqiao <villardqiao@gmail.com>
2017-08-01 21:25:43 -0400

    Update Test.go

--- Test/Test.go
+++ Test/Test.go
@@ -1,3 +1,20 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
 package main
 
 import (
@@ -17,7 +34,119 @@ type ChaincodePrototype struct {
 func (t *ChaincodePrototype) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Printf("Init called, initializing chaincode")
 	
+	return nil, nil
+}
+
+// Transaction makes payment of X units from A to B
+func (t *ChaincodePrototype) transaction(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Running invoke")
+
+	user := "hyperledgertest1@gmail.com"
+	password := "George2017"
+	host := "smtp.gmail.com:587"
+	to1 := "hyperledgertest2@gmail.com"
+	to2 := "hyperledgertest3@gmail.com"
+
+	subject := "Indentification"
+
+	body := `
+		<html>
+		<body>
+		<h1>
+		"Have you finished your work yet?"
+		</h1>
+		<button name="yes" type="submit" style="height:50px;width:100px;font-size:30px">Yes</button>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<button name="no" type="submit" style="height:50px;width:100px;font-size:30px">No</button>
+		</body>
+		</html>
+		`
+	fmt.Println("Sending email")
+
+	err1 := SendToMail(user, password, host, to1, subject, body, "html")
+	if err1 != nil {
+		fmt.Println("Send email1 error!")
+		fmt.Println(err1)
+	} else {
+		fmt.Println("Send email1 success!")
+	}
+
+	err2 := SendToMail(user, password, host, to2, subject, body, "html")
+	if err2 != nil {
+		fmt.Println("Send email2 error!")
+		fmt.Println(err2)
+	} else {
+		fmt.Println("Send email2 success!")
+	}
+
+	var A, B string    // Entities
+	var Aval, Bval int // Asset holdings
+	var X int          // Transaction value
+	var err error
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+
+	A = args[0]
+	B = args[1]
+
+	// Get the state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if Avalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	Aval, _ = strconv.Atoi(string(Avalbytes))
+
+	Bvalbytes, err := stub.GetState(B)
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if Bvalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	Bval, _ = strconv.Atoi(string(Bvalbytes))
+
+	// Perform the execution
+	X, err = strconv.Atoi(args[2])
+	Aval = Aval - X
+	Bval = Bval + X
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+
+	// Write the state back to the ledger
+	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// Deletes an entity from state
+func (t *ChaincodePrototype) delete(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Running delete")
 	
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+
+	A := args[0]
+
+	// Delete the key from the state in ledger
+	err := stub.DelState(A)
+	if err != nil {
+		return nil, errors.New("Failed to delete state")
+	}
+
 	return nil, nil
 }
 
@@ -27,10 +156,19 @@ func (t *ChaincodePrototype) Invoke(stub shim.ChaincodeStubInterface, function s
 	fmt.Printf("Invoke called, determining function")
 	
 	// Handle different functions
-	if function == "init" {
+	if function == "invoke" {
+		// Transaction makes payment of X units from A to B
+		fmt.Printf("Function is invoke")
+		return t.transaction(stub, args)
+	} else if function == "init" {
 		fmt.Printf("Function is init")
 		return t.Init(stub, function, args)
+	} else if function == "delete" {
+		// Deletes an entity from its state
+		fmt.Printf("Function is delete")
+		return t.delete(stub, args)
 	}
+
 	return nil, errors.New("Received unknown function invocation")
 }
 
@@ -38,9 +176,17 @@ func (t* ChaincodePrototype) Run(stub shim.ChaincodeStubInterface, function stri
 	fmt.Printf("Run called, passing through to Invoke (same function)")
 	
 	// Handle different functions
-	if function == "init" {
+	if function == "invoke" {
+		// Transaction makes payment of X units from A to B
+		fmt.Printf("Function is invoke")
+		return t.transaction(stub, args)
+	} else if function == "init" {
 		fmt.Printf("Function is init")
 		return t.Init(stub, function, args)
+	} else if function == "delete" {
+		// Deletes an entity from its state
+		fmt.Printf("Function is delete")
+		return t.delete(stub, args)
 	}
 
 	return nil, errors.New("Received unknown function invocation")
@@ -50,7 +196,34 @@ func (t* ChaincodePrototype) Run(stub shim.ChaincodeStubInterface, function stri
 func (t *ChaincodePrototype) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Printf("Query called, determining function")
 	
-	return nil, nil
+	if function != "query" {
+		fmt.Printf("Function is query")
+		return nil, errors.New("Invalid query function name. Expecting \"query\"")
+	}
+	var A string // Entities
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
+	}
+
+	A = args[0]
+
+	// Get the state from the ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	if Avalbytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return Avalbytes, nil
 }
 
 func SendToMail(user, password, host, to, subject, body, mailtype string) error {
